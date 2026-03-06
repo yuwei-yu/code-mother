966238f303828778170945ee6033e5b5cbad7b8d
yuwei-yu <3414048190@qq.com>
2026-03-06 14:46:01 +0800

    使用openapi生成请求api

---
+++ front/yw-code-mother/src/api/index.ts
@@ -0,0 +1,18 @@
+// @ts-ignore
+/* eslint-disable */
+// API 更新时间：
+// API 唯一标识：
+import * as workflowSseController from './workflowSseController'
+import * as userController from './userController'
+import * as chatHistoryController from './chatHistoryController'
+import * as appController from './appController'
+import * as staticResourceController from './staticResourceController'
+import * as healthController from './healthController'
+export default {
+  workflowSseController,
+  userController,
+  chatHistoryController,
+  appController,
+  staticResourceController,
+  healthController,
+}
