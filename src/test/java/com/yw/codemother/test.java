dbeb8998996e3831689a7fc1d1fb5b2923cd9296
yuwei-yu <3414048190@qq.com>
2026-03-10 18:43:55 +0800

    前端 --完善配置

---
+++ src/test/java/com/yw/codemother/test.java
@@ -0,0 +1,8 @@
+package com.yw.codemother;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println(System.getProperty("user.dir") + "/tmp/code_deploy");
+
+    }
+}
